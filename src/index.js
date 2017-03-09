6b934c8e67975d15d0995ff22ca47e08ba075658
EGOIST <0x142857@gmail.com>
2017-03-09 22:29:25 +0800

    expose match

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 export default function ({
   sandbox = 'allow-scripts allow-same-origin',
   prepend = '',
-  append = ''
+  append = '',
+  match = /^`{4}.*?\n([\s\S]*?)\n`{4}/gm
 } = {}) {
   function escapeHtml(text) {
     return text
@@ -13,10 +14,8 @@ export default function ({
   }
 
   return ({ beforeParse }) => {
-    const re = /^`{4}.*?\n([\s\S]*?)\n`{4}/gm
-
     beforeParse(raw => {
-      return raw.replace(re, (_, p1) => {
+      return raw.replace(match, (_, p1) => {
         return `<iframe sandbox="${sandbox}" srcdoc="${escapeHtml(prepend)}${escapeHtml(p1)}${escapeHtml(append)}"></iframe>`
       })
     })
